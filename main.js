7837762b8bd6c0908a8f0ac4029ca2722cb21fcb
Daniel <djdanilko02@gmail.com>
2026-04-29 19:33:41 +0200

    added_DOMelements

--- main.js
+++ main.js
@@ -7,8 +7,36 @@ const array = [];
 
 form.addEventListener("submit", evt => {
     evt.preventDefault(); 
-array.push(input.value)
+array.push({
+  text: input.value,
+  completed: false
+});
+renderList();
 console.log(array)
 input.value = "";
 });
 
+function renderList() {
+    list.innerHTML = "";
+
+    array.forEach((item, index) => {
+        const li = document.createElement("li");
+        list.appendChild(li)
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        li.appendChild(checkbox)
+
+        const span = document.createElement("span");
+        span.textContent = item.text;
+        li.appendChild(span)
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        li.appendChild(deleteBtn)
+
+        const editBtn = document.createElement("button");
+        editBtn.textContent = "Edit";
+        li.appendChild(editBtn)
+    })
+}
